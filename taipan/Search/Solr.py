6fbd34a783c307868b3c349ba9076ce4cb257011
Ivan Ermilov <earthquakesan@gmail.com>
2015-09-02 18:00:39 +0200

    added test for SolrSearch

--- taipan/Search/Solr.py
+++ taipan/Search/Solr.py
@@ -4,7 +4,6 @@ from taipan.Config.Solr import solrUri
 from taipan.Config.Solr import solrShortAbstractsCore
 
 class SolrSearch(object):
-
     def __init__(self):
         self.shortAbstractsSolr = solr.Solr(solrUri + solrShortAbstractsCore)
 
@@ -13,11 +12,12 @@ class SolrSearch(object):
             Document in solrShortAbstractsCore has two fields:
             a standard "id" field and
             comment_en field indexed as English text
-            Returns solr.core.Response object
+            Returns top 10 results:
+            {
+                 u'_version_': 1511204700087648275L,
+                 u'comment_en': [u'Damion Scott is a comic book artist and writer, known for his work on books such as Batman, Robin, and Batgirl, Web of Spider-Man, and Duppy. He splits his time between New York and Tokyo, where he founded an art studio that publishes a Japanese comic called Saturday Morning Cartoons or SAM-C.'],
+                 u'id': u'http://dbpedia.org/resource/Damion_Scott',
+                 u'score': 2.5929914}
+            }
         """
-        return self.shortAbstractsSolr.select("comment_en:"+lookupString)
-
-if __name__ == "__main__":
-    solrSearch = SolrSearch()
-    robin = solrSearch.getInstance("Robin")
-    import ipdb; ipdb.set_trace()
+        return self.shortAbstractsSolr.select("comment_en:\""+lookupString+"\"").results
---
+++ taipan/Tests/Search/SolrTest.py
@@ -0,0 +1,16 @@
+import unittest
+
+from taipan.Utils.Sampling import Sampler
+from taipan.Relational.Parsers import MannheimParser
+from taipan.Search.Solr import SolrSearch
+
+class SolrTestCase(unittest.TestCase):
+    def setUp(self):
+        self.solr = SolrSearch()
+
+    def testGetInstances(self):
+        instances = self.solr.getInstances("Batman & Robin")
+        self.assertTrue('id' in instances[0], msg="Instance should have 'id' key")
+        self.assertTrue('_version_' in instances[0], msg="Instance should have '_version_' key")
+        self.assertTrue('comment_en' in instances[0], msg="Instance should have 'comment_en' key")
+        self.assertTrue('score' in instances[0], msg="Instance should have 'score_en' key")
