c7088b9dd2392a818996293cf0a0c850c6f9f904
Ivan Ermilov <earthquakesan@gmail.com>
2015-10-29 22:52:31 +0100

    annotated a bit more tables for subject column identification

--- scripts/getPropertiesForTable.py
+++ scripts/getPropertiesForTable.py
@@ -6,9 +6,9 @@ tableId = sys.argv[1]
 from taipan.T2D.Table import T2DTable
 table = T2DTable(tableId)
 from taipan.Learning.SubjectColumnIdentification.DistantSupervisionIdentifier import DistantSupervisionIdentifier
-dlIdentifier = DistantSupervisionIdentifier()
-subjectColumn = dlIdentifier.identifySubjectColumn(table)
-print subjectColumn
+#dlIdentifier = DistantSupervisionIdentifier()
+#subjectColumn = dlIdentifier.identifySubjectColumn(table)
+#print subjectColumn
 print table.properties
 print table.classes
 print table.table[0:10]
