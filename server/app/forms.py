a5ea1958af6ed1319bc0b31724282613c7d382e2
Ivan Ermilov <earthquakesan@gmail.com>
2015-11-13 22:04:21 +0100

    fix form

--- server/app/forms.py
+++ server/app/forms.py
@@ -6,7 +6,7 @@ class SubjectColumnAnnotatorForm(Form):
     subjectColumn = TextField('Subject Column: <span class="label label-default" id="subjectColumnValue">Click to select</span>')
     noSubjectColumn = BooleanField('No Subject Column!')
     #tableType = SelectMultipleField('Table Type', choices=["Normal", "Vertical", "Layout", "Not English"])
-    tableType = RadioField('Table Type', default="Normal", choices=[("Normal","Normal"), ("Layout", "Layout"), ("Vertical", "Vertical"), ("NotEnglish", "Not English"), ("Statistical", "Statistical")])
+    tableType = RadioField('Table Type', default="Normal", choices=[("Normal","Normal"), ("Layout", "Layout"), ("Vertical", "Vertical"), ("Statistical", "Statistical"), ("NotEnglish", "Not English")])
 
 class UsernameForm(Form):
     username = TextField('Your name', [Regexp("^[a-zA-Z0-9_]*$", message="only alphanumerical usernames are allowed"), Required(message="please provide a username")])
